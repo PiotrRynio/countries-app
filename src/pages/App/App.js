a8286b5e0251933a23ae9deef4e616a5b01123ab
PiotrWR <pwrynio@gmail.com>
2021-09-02 18:37:53 +0200

    Add structure

---
+++ src/pages/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from '../../components/Navbar/Navbar';
+import Home from '../Home/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default App;

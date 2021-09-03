4cdfb92c2a65c7eb0fb1feb40bbf32215c454e49
PiotrWR <pwrynio@gmail.com>
2021-09-03 10:25:14 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,25 +3,29 @@ import { errorCodes, useCountriesNames } from '../../api/apiHooks';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import styles from './Home.module.scss';
 
-const Navbar = () => {
+const Home = () => {
   const { isLoading, error, data } = useCountriesNames('united');
 
-  if (isLoading) return <SectionTitle isAlert={true}>'Loading...'</SectionTitle>;
+  let title = <SectionTitle>Countries list:</SectionTitle>;
+
+  if (isLoading) title = <SectionTitle isAlert={true}>'Loading...'</SectionTitle>;
 
   if (error) {
     if (error.message === errorCodes.NOT_FOUND)
-      return (
-        <SectionTitle isAlert={true}>'countries not found - modify the search field'</SectionTitle>
+      title = (
+        <SectionTitle isAlert={true}>
+          'Not found countries matching given search input'
+        </SectionTitle>
       );
-    return <SectionTitle isAlert={true}>'api server error'</SectionTitle>;
+    title = <SectionTitle isAlert={true}>'api server error'</SectionTitle>;
   }
 
   return (
     <section className={styles.home}>
-      <SectionTitle>Countries list:</SectionTitle>
-      <section className={styles.home__body}>{data[0].name}</section>
+      {title}
+      {data ? <section className={styles.home__body}>{data[0].name}</section> : ''}
     </section>
   );
 };
 
-export default Navbar;
+export default Home;

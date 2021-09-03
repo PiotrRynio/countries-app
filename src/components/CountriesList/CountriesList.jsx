8ba73fa1818e40e74c89c7c916a89b25432cc33b
PiotrWR <pwrynio@gmail.com>
2021-09-03 14:52:49 +0200

    Add action on click item

--- src/components/CountriesList/CountriesList.jsx
+++ src/components/CountriesList/CountriesList.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import styles from './CountriesList.module.scss';
 import { BsChevronRight } from 'react-icons/all';
+import { useHistory } from 'react-router-dom';
 
 const CountriesList = ({ countriesData = [] }) => {
+  let history = useHistory();
+
   if (!countriesData) return;
+
+  function handleClick(param) {
+    history.push(`/countries/${param}`);
+  }
 
   return (
     <ul className={styles.countriesList}>
       {countriesData.map(({ name }) => (
-        <li key={name} className={styles.countriesList__item}>
+        <li key={name} className={styles.countriesList__item} onClick={() => handleClick(name)}>
           <h3 className={styles.countriesList__itemTitle}>{name}</h3>
           <BsChevronRight className={styles.countriesList__arrow} />
         </li>

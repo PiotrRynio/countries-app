a056cf043b4943aad0e3e02010a6363567f6a1e4
PiotrWR <pwrynio@gmail.com>
2021-09-04 02:38:45 +0200

    Fix no page view of paginate

--- src/components/Paginate/Paginate.jsx
+++ src/components/Paginate/Paginate.jsx
@@ -6,22 +6,25 @@ const Paginate = ({ pageCount = 1, forcePage = 1, onPageChange = (selected) => {
   const onPageChangeHandle = ({ selected }) => {
     onPageChange(selected + 1);
   };
+
   return (
-    <nav className="paginate">
-      <ReactPaginate
-        previousLabel={'<'}
-        nextLabel={'>'}
-        breakLabel={'...'}
-        breakClassName={'break-me'}
-        pageCount={pageCount}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={4}
-        onPageChange={onPageChangeHandle}
-        containerClassName={'pagination'}
-        forcePage={Number(forcePage - 1)}
-        activeClassName={'active'}
-      />
-    </nav>
+    Boolean(pageCount) && (
+      <nav className="paginate">
+        <ReactPaginate
+          previousLabel={'<'}
+          nextLabel={'>'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={4}
+          onPageChange={onPageChangeHandle}
+          containerClassName={'pagination'}
+          forcePage={Number(forcePage - 1)}
+          activeClassName={'active'}
+        />
+      </nav>
+    )
   );
 };
 

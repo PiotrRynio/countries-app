68f5b28e0f445bf752ed3536546d0025746bb196
PiotrWR <pwrynio@gmail.com>
2021-09-06 01:26:34 +0200

    Add test in Home page

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import { handlers } from '../../mock/msw/handlers/countriesNames/handlers';
 import Home from './Home';
@@ -19,6 +19,20 @@ describe('Home page: ', () => {
     const headerTitle = screen.getByRole('heading');
 
     const searchedText = /Loading/i;
+    expect(headerTitle).toHaveTextContent(searchedText);
+  });
+
+  test(`when country names are fetched, then loading text is changed to other`, async () => {
+    const { componentWithWrappers } = addTestWrappers({
+      route: undefined,
+      toBeWrappedComponent: <Home />,
+    });
+    render(componentWithWrappers);
+    const headerTitle = screen.getByRole('heading');
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+
+    const searchedText = /Countries list/i;
     expect(headerTitle).toHaveTextContent(searchedText);
   });
 });

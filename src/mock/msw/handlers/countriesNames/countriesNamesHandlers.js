0c009597e9764b2234ee8d2c4ba8a9c1eaf34764
PiotrWR <pwrynio@gmail.com>
2021-09-05 17:03:51 +0200

    Fix wrong url in handlers

--- src/mock/msw/handlers/countriesNames/countriesNamesHandlers.js
+++ src/mock/msw/handlers/countriesNames/countriesNamesHandlers.js
@@ -3,20 +3,16 @@ import { countriesNamesHandlerResponses } from './countriesNamesHandlerResponses
 
 const API_URL = 'https://restcountries.eu/rest/v2';
 
-const allCountriesNamesHandler = rest.get(`${API_URL}/:searchedPhrase`, (req, res, ctx) => {
+const allCountriesNamesHandler = rest.get(`${API_URL}/all`, (req, res, ctx) => {
   const { searchParams } = req.url;
   const fields = searchParams.get('fields');
-  const searchedPhrase = req.params.searchedPhrase;
+  // const searchedPhrase = req.params.searchedPhrase;
 
   if (fields !== 'name') {
     return res(ctx.status(404));
   }
 
-  if (searchedPhrase === 'all') {
-    return res(ctx.status(200), ctx.json(countriesNamesHandlerResponses));
-  }
-
-  return res(ctx.status(404));
+  return res(ctx.status(200), ctx.json(countriesNamesHandlerResponses));
 });
 
 export const countriesNamesHandlers = [allCountriesNamesHandler];

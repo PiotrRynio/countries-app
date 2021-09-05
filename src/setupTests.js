a10733a013d2ef3c799e000b669c4b76bcc017d8
PiotrWR <pwrynio@gmail.com>
2021-09-06 00:09:31 +0200

    Add wrapping function with router in setupTest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,27 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+global.addTestWrappers = ({ toBeWrappedComponent, route = undefined, ...others }) => {
+  const { componentWithRouter, history } = addRouterWrapper(toBeWrappedComponent, route);
+
+  return {
+    componentWithWrappers: componentWithRouter,
+    history: history,
+    others: others,
+  };
+};
+
+const addRouterWrapper = (toBeWrappedComponent, route) => {
+  const history = createMemoryHistory();
+  if (route) {
+    history.push(route);
+  }
+  const componentWitRouter = <Router history={history}> toBeWrappedComponent</Router>;
+  return {
+    componentWithRouter: componentWitRouter,
+    history: history,
+  };
+};

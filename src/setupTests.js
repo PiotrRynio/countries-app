5d93744a9ab92521e808e89a24a7abe93f46e653
PiotrWR <pwrynio@gmail.com>
2021-09-06 00:31:03 +0200

    Add Query Client Provider Wrapper

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,25 +5,37 @@
 import '@testing-library/jest-dom';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 global.addTestWrappers = ({ toBeWrappedComponent, route = undefined, ...others }) => {
   const { componentWithRouter, history } = addRouterWrapper(toBeWrappedComponent, route);
+  const { componentWitQueryClientProvider } = addQueryClientProviderWrapper(componentWithRouter);
 
   return {
-    componentWithWrappers: componentWithRouter,
+    componentWithWrappers: componentWitQueryClientProvider,
     history: history,
     others: others,
   };
 };
 
-const addRouterWrapper = (toBeWrappedComponent, route) => {
+const addRouterWrapper = (toBeWrappedComponent, route = undefined) => {
   const history = createMemoryHistory();
   if (route) {
     history.push(route);
   }
-  const componentWitRouter = <Router history={history}> toBeWrappedComponent</Router>;
+  const componentWitRouter = <Router history={history}> {toBeWrappedComponent}</Router>;
   return {
     componentWithRouter: componentWitRouter,
     history: history,
+  };
+};
+
+const addQueryClientProviderWrapper = (toBeWrappedComponent) => {
+  const queryClient = new QueryClient();
+  const componentWitQueryClientProvider = (
+    <QueryClientProvider client={queryClient}> {toBeWrappedComponent}</QueryClientProvider>
+  );
+  return {
+    componentWitQueryClientProvider: componentWitQueryClientProvider,
   };
 };

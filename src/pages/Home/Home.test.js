29da12206bde1e8a9e7eeb5cc6be93a4e0767637
PiotrWR <pwrynio@gmail.com>
2021-09-06 00:58:10 +0200

    Add loading screen test in Home Page - integration test

---
+++ src/pages/Home/Home.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { handlers } from '../../mock/msw/handlers/countriesNames/handlers';
+import Home from './Home';
+
+describe('Home page: ', () => {
+  const server = setupServer(...handlers);
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  test(`when country names are not fetched yet, then displays loading screens`, async () => {
+    const { componentWithWrappers } = addTestWrappers({
+      route: undefined,
+      toBeWrappedComponent: <Home />,
+    });
+    render(componentWithWrappers);
+    const headerTitle = screen.getByRole('heading');
+
+    const searchedText = /Loading/i;
+    expect(headerTitle).toHaveTextContent(searchedText);
+  });
+});
